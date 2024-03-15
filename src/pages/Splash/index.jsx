91ae36e1f17a76a1dd11074a324dfa735ddbe64f
KrisnaMurti-21 <krisnamr27@gmail.com>
2024-03-15 15:54:48 +0700

    add routing and global color

---
+++ src/pages/Splash/index.jsx
@@ -0,0 +1,35 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React, {useEffect} from 'react';
+import {ILLogo} from '../../assets';
+import {colors} from '../../utils';
+
+const Splash = ({navigation}) => {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.replace('GetStarted');
+    }, 3000);
+  }, []);
+  return (
+    <View style={styles.page}>
+      <ILLogo />
+      <Text style={styles.title}>My Doctor</Text>
+    </View>
+  );
+};
+
+export default Splash;
+
+const styles = StyleSheet.create({
+  page: {
+    backgroundColor: colors.white,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontFamily: 'Nunito-SemiBold',
+    color: colors.text.primary,
+    marginTop: 20,
+  },
+});
