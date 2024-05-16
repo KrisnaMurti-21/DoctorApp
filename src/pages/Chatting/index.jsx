f9ff45b2f4d01ea9f2d5e9e4dfcc88c96bfe9868
KrisnaMurti-21 <krisnamr27@gmail.com>
2024-05-16 12:16:36 +0700

    prepare for messages page

--- src/pages/Chatting/index.jsx
+++ src/pages/Chatting/index.jsx
@@ -1,23 +1,36 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {ChatItem, Header, InputChat} from '../../components';
 import {colors, fonts} from '../../utils';
 
-const Chatting = ({navigation}) => {
+const Chatting = ({navigation, route}) => {
+  const dataDoctor = route.params;
   return (
     <View style={styles.page}>
       <Header
-        title="Naoirobi Putri Hayza"
+        title={dataDoctor.data.fullName}
+        desc={dataDoctor.data.profession}
+        photo={{uri: dataDoctor.data.photo}}
         type={'dark-profile'}
         onPress={() => navigation.goBack()}
       />
       <View style={styles.content}>
-        <Text style={styles.chatDate}>Senin, 21 Maret 2020</Text>
-        <ChatItem isMe />
-        <ChatItem />
-        <ChatItem isMe />
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <Text style={styles.chatDate}>Senin, 21 Maret 2020</Text>
+          <ChatItem isMe />
+          <ChatItem />
+          <ChatItem isMe />
+        </ScrollView>
       </View>
-      <InputChat />
+      <InputChat
+        value={'Hallo'}
+        onChangeText={() => {
+          alert('input tap');
+        }}
+        onButtonPress={() => {
+          alert('button press');
+        }}
+      />
     </View>
   );
 };
